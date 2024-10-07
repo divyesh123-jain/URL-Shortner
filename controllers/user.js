e404d565c178f7f0ecb2786da26c714e83fa8013
divyesh123-jain <divyeshjain318@gmail.com>
2024-10-07 14:24:58 +0530

    implememnted user login routes

--- controllers/user.js
+++ controllers/user.js
@@ -12,6 +12,22 @@ async function handleUserSignup(req ,res){
     res.json({message: 'User created'})
 }
 
+
+async function handleUserLogin(req ,res){
+
+    const { email , password} = req.body
+    await User.findOne(
+        {
+            email,
+            password
+        }
+    )
+    if(!User ) return res.json({message: 'Invalid Email or Password'})
+
+
+    res.json({message: 'User Login'})
+}
 module.exports = {
-    handleUserSignup
+    handleUserSignup,
+    handleUserLogin
 }
